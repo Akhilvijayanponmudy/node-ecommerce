d225add61bd9b9f0f9ca59f7327a73eee7c7ab21
Akhil <akhil.v@webandcrafts.com>
2024-03-25 18:38:54 +0530

    added addrfess adding functionalitiues

--- User/account/accountController.js
+++ User/account/accountController.js
@@ -8,25 +8,46 @@ const addressView = (req, res) => {
 }
 
 const addAddress = async (req, res) => {
-    const userId = req.user.userId;
 
-    res.status(201).json({successful: true, message: "Address added successfully"});
+    try {
+        const userId = req.user.userId;
+        let addressSchema = await Address.findOne({ userId });
 
-    const { name, addressLine1, addressLine2, city, state, zip } = req.body.data;
-    console.log(name);
-    if (!name || !addressLine1 || !addressLine2 || !city || !state || !zip) {
-        res.status(403).json({ successful: false, message: "missing details" });
+        if (!addressSchema) {
+            const { name, addressLine1, addressLine2, city, state, zip } = req.body.data;
+            if (!name || !addressLine1 || !addressLine2 || !city || !state || !zip) {
+                res.status(403).json({ successful: false, message: "missing details" });
+            }
+            if (!userId) {
+                res.status(403).json({ successful: false, message: "token validation falied" });
+            }
+            const address = { userId, name, addressLine1, addressLine2, city, state, zip };
+            const newAddress = new Address({
+                userId: userId,
+                items: [address]
+            });
+            await newAddress.save();
+            res.status(201).json({ successful: true, message: "New Address added successfully" });
+        }
+        else {
+            const { name, addressLine1, addressLine2, city, state, zip } = req.body.data;
+            if (!name || !addressLine1 || !addressLine2 || !city || !state || !zip) {
+                res.status(403).json({ successful: false, message: "missing details" });
+            }
+            if (!userId) {
+                res.status(403).json({ successful: false, message: "token validation falied" });
+            }
+            const address = { name, addressLine1, addressLine2, city, state, zip };
+            addressSchema.items.push(address);
+            await addressSchema.save();
+            res.status(201).json({ successful: true, message: "New Address added successfully" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(403).json({ successful: false, message: "Technical error" });
+
     }
-    if (!userId) {
-        res.status(403).json({ successful: false, message: "token validation falied" });
-    }
-    const address ={ userId, name, addressLine1, addressLine2, city, state, zip };
-    const newAddress = new Address({ 
-        userId:userId,
-        items: [address]
-    });
-    await newAddress.save();
-    res.status(201).json({successful: true, message: "Address added successfully"});
 }
 
 module.exports = {
--- User/account/accountRoutes.js
+++ User/account/accountRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const verifyJWT = require('../validators/verifyJWT')
-const { accountView, addressView ,addAddress} = require('./accountController')
+const { accountView, addressView, addAddress } = require('./accountController')
 
 
 router.get('/', accountView);
 router.get('/address', addressView);
-router.post('/address/add',verifyJWT,addAddress);
+router.post('/address/add', verifyJWT, addAddress);
 
 
 module.exports = router;
